0fd51fbb8fa3aee15803df7d1d5bed9782f79b21
alordash <alordash@mail.ru>
2023-06-11 16:55:40 +0300

    Optimization

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,33 @@ fn shuffle_vec<T>(vec: &mut Vec<T>) {
     }
 }
 
-fn gen_shuffled_vec(size: usize) -> Vec<usize> {
-    let mut vec: Vec<usize> = (0..size).collect();
+fn gen_shuffled_vec(size: i32) -> Vec<i32> {
+    let mut vec: Vec<i32> = (0..size).collect();
     shuffle_vec(&mut vec);
     vec
 }
 
 struct Pendings {
     seed: u64,
-    orders: Vec<Vec<usize>>,
+    orders: Vec<Vec<i32>>,
 }
 
 impl Pendings {
-    pub fn new(seed: u64, orders_count: usize, order_size: usize) -> Pendings {
+    pub fn new(seed: u64, orders_count: i32, order_size: i32) -> Pendings {
         srand(seed);
         let gen_shuffled_vec = |_| gen_shuffled_vec(order_size);
-        let orders: Vec<Vec<usize>> = (0..orders_count).map(gen_shuffled_vec).collect();
+        let orders: Vec<Vec<i32>> = (0..orders_count).map(gen_shuffled_vec).collect();
         Pendings { seed, orders }
     }
 
-    pub fn orders(&self) -> &Vec<Vec<usize>> {
+    pub fn orders(&self) -> &Vec<Vec<i32>> {
         &self.orders
     }
 
     pub fn update_seed(&mut self, new_seed: u64) {
         self.seed = new_seed;
         srand(self.seed);
-        let gen_shuffled_vec = |_| gen_shuffled_vec(self.orders[0].len());
+        let gen_shuffled_vec = |_| gen_shuffled_vec(self.orders[0].len() as i32);
         self.orders = (0..self.orders.len()).map(gen_shuffled_vec).collect();
     }
 }
@@ -46,18 +46,22 @@ fn update_image(img: &mut Image, step: usize, pendings: &Pendings) {
     let width = img.width();
     let height = img.height();
 
+    let f_step = step as f32;
+    let f_width = width as f32;
+    let f_height = height as f32;
+
     for x in 0..width as u32 {
         for y in 0..height as u32 {
             img.set_pixel(x, y, PIXEL_LIGHT);
         }
     }
 
-    let col = (width as f32 - (step as f32 / height as f32).ceil()).max(0.0) as usize;
+    let col = (f_width - (f_step / f_height).ceil()).max(0.0) as usize;
     for x in (col..width).rev() {
         for y in 0..height {
             let offset = step as i32
                 - (width - x - 1) as i32 * height as i32
-                - pendings.orders()[x].iter().position(|v| *v == y).unwrap() as i32;
+                - pendings.orders()[x][y] as i32;
             if offset > 0 {
                 img.set_pixel(x as u32, y as u32, PIXEL_DARK);
                 if (x + offset as usize) < width {
@@ -73,13 +77,13 @@ const HELP: &'static str =
 
 #[macroquad::main("Tetris progressbar")]
 async fn main() {
-    let width: u16 = 50;
-    let height: u16 = 25;
+    let width: u16 = 58;
+    let height: u16 = 30;
 
     let mut pendings = Pendings::new(
         gen_range(u64::MIN, u64::MAX),
-        width as usize,
-        height as usize,
+        width as i32,
+        height as i32,
     );
 
     let mut img = Image::gen_image_color(width, height, PIXEL_LIGHT);
